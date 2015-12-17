bf3cf4d8c4e397137b30514d8cd4dc3af2c5b79c
daariff <aariff.deen@gmail.com>
2015-12-17 13:30:55 -0800

    Added user and food routes

--- chefu/routes/food.js
+++ chefu/routes/food.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 router.get('/', function(req, res) {
   var db = req.db;
-  var users = db.get('userList');
+  var users = db.get('foodList');
   users.find( {}, {}, function(e,docs){
     res.send(docs);
   });
@@ -12,7 +13,7 @@ router.get('/', function(req, res) {
 router.get('/:id', function(req, res) {
   var id = req.params.id;
   var db = req.db;
-  var users = db.get('userList');
+  var users = db.get('foodList');
   res.send(users[id]);
 });
 
--- chefu/server.js
+++ chefu/server.js
@@ -1,17 +1,21 @@
 var express = require('express');
+
+
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//var favicon = require('serve-favicon');
+//var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+//var bodyParser = require('body-parser');
+
+// Route Setup
+var index = require('./routes/index');
+var users = require('./routes/users');
+var food = require('./routes/food');
 
 // Database
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/chefu');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -19,16 +23,24 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+
+// Serve Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// Index Route
+app.get('/', index):
+
+// User Route
+app.get('/users', users);
+
+// Food Route
+app.get('/food', food);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
